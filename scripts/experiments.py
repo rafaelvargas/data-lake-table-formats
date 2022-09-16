6cde78bfd6c66490294aa3a27d73db1aa6a54ddb
Rafael Vargas <rafaelflvargas@gmail.com>
2022-09-15 23:13:28 -0300

    Update the spark configurations and implement measurement of the execution time

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,17 +1,31 @@
 
 from datetime import datetime
+import time
 
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 
 import tables
 
+ENV = 'dev'
 AWS_JARS=',org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.12.300'
+
 
 class Experiment:
     def __init__(self, table_format: str, scale_in_gb: int, confs: list, path: str):
         self._table_format = table_format
         self._confs = confs
+        if ENV == 'dev':
+            self._confs += [
+                ('spark.hadoop.fs.s3a.access.key', 'minioadmin'),
+                ('spark.hadoop.fs.s3a.secret.key', 'minioadmin'),
+                ('spark.hadoop.fs.s3a.endpoint', 'http://127.0.0.1:9000'),
+                ('spark.hadoop.fs.s3a.connection.ssl.enabled', 'false'),
+                ('spark.sql.catalogImplementation', 'hive'),
+                ('hive.metastore.uris', 'thrift://localhost:9083'), 
+                ('spark.sql.warehouse.dir', f'{path}/databases/'),
+                ('spark.sql.execution.pyarrow.enabled', 'true')
+            ]
         self._spark_session = self._create_spark_session()
         now = datetime.now()
         self._database_name = f"{scale_in_gb}gb_{self._table_format}" 
@@ -57,22 +71,19 @@ class Experiment:
             """,
             "delta": ""
         }    
-            
-        self._spark_session.sql(f"DROP TABLE IF EXISTS `{self._database_name}`.`{table}`;")
-        self._spark_session.sql(
-            f"""
-            CREATE TABLE `{self._database_name}`.`{table}` 
+        
+        self._run_sql(f"DROP TABLE IF EXISTS `{self._database_name}`.`{table}`;")
+        self._run_sql(f"""
+            CREATE EXTERNAL TABLE `{self._database_name}`.`{table}` 
             USING {self._table_format}
             { partition_string }
             { options_string[self._table_format] }
             LOCATION '{target_location}'
             SELECT * FROM `parquet`.`s3a://datasets/load_{table}.snappy.parquet`;
-            """
-        )
+        """)
 
     def _update_data(self, table: str = 'fact_daily_usage_by_user', percentage_to_update: int = 8):
-        self._spark_session.sql(
-            f"""
+        self._run_sql(f"""
             MERGE INTO `{self._database_name}`.`{table}` t
             USING (SELECT * FROM parquet.`s3a://datasets/update_{percentage_to_update}_{table}.snappy.parquet`) s
                 ON t.date = s.date
@@ -85,11 +96,17 @@ class Experiment:
                 t.duration_in_seconds = s.duration_in_seconds,
                 t.number_of_logins = s.number_of_logins,
                 t.number_of_songs_played = s.number_of_songs_played;
-            """
-        )
+        """)
+
+    def _run_sql(self, sql: str):
+        start = time.time()
+        df = self._spark_session.sql(sql)
+        _ = df.collect()
+        end = time.time()
+        print(f"Execution time: {end - start}")
     
     def _query_data(self, table: str = 'fact_daily_usage_by_user'):
-        self._spark_session.sql(f"""
+        self._run_sql(f"""
             SELECT 
                 date, 
                 COUNT(DISTINCT user_id) daily_active_users, 
@@ -99,8 +116,8 @@ class Experiment:
             FROM `{self._database_name}`.`{table}`
             GROUP BY 
                 date;
-        """).show()
-        self._spark_session.sql(f"""
+        """)
+        self._run_sql(f"""
             SELECT 
                 c.name country, 
                 COUNT(DISTINCT user_id) number_of_active_users
@@ -111,11 +128,11 @@ class Experiment:
                 c.name
             ORDER BY number_of_active_users DESC
             LIMIT 1;
-        """).show()
+        """)
 
     def _load_tables(self):
-        self._spark_session.sql(f"DROP DATABASE IF EXISTS {self._database_name} CASCADE;")
-        self._spark_session.sql(f"CREATE DATABASE IF NOT EXISTS {self._database_name};")
+        self._run_sql(f"DROP DATABASE IF EXISTS {self._database_name} CASCADE;")
+        self._run_sql(f"CREATE DATABASE IF NOT EXISTS {self._database_name} LOCATION '{self._database_path}';")
         for table_name in tables.DEFINITIONS.keys():
             self._load_data(table=table_name)
 
@@ -137,18 +154,9 @@ class IcebergExperiment(Experiment):
     ):
         self._scale_in_gb = scale_in_gb
         dependencies = f"org.apache.iceberg:iceberg-spark-runtime-3.2_{scala_version}:{iceberg_version}"
-        dependencies += AWS_JARS
+        if ENV == 'dev': dependencies += AWS_JARS
         confs = [
             ("spark.jars.packages", dependencies),
-            ("spark.hadoop.fs.s3a.access.key", "minioadmin"),
-            ("spark.hadoop.fs.s3a.secret.key", "minioadmin"),
-            ("spark.hadoop.fs.s3a.endpoint", "http://127.0.0.1:9000"),
-            ("spark.hadoop.fs.s3a.connection.ssl.enabled", "false"),
-            ("spark.sql.catalog.spark_catalog.type", "hive"),
-            ("spark.sql.catalog.spark_catalog.uri", "thrift://localhost:9083"),
-            # ("spark.sql.catalog.spark_catalog.warehouse", "s3a://iceberg/"),
-            ("spark.sql.execution.pyarrow.enabled", "true"),
-            # ("spark.sql.warehouse.dir", "s3a://iceberg/"),
             ("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions"),
             ("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
         ]
@@ -164,19 +172,9 @@ class DeltaExperiment(Experiment):
     ):
         self._scale_in_gb = scale_in_gb
         dependencies = f"io.delta:delta-core_{scala_version}:{delta_version},io.delta:delta-contribs_{scala_version}:{delta_version},io.delta:delta-hive_{scala_version}:0.2.0"
-        dependencies += AWS_JARS
+        if ENV == 'dev': dependencies += AWS_JARS
         confs = [
             ("spark.jars.packages", dependencies),
-            ('spark.hadoop.fs.s3a.access.key', 'minioadmin'),
-            ('spark.hadoop.fs.s3a.secret.key', 'minioadmin'),
-            ('spark.hadoop.fs.s3a.endpoint', 'http://127.0.0.1:9000'),
-            ('spark.hadoop.fs.s3a.connection.ssl.enabled', 'false'),
-            ('spark.sql.catalogImplementation', 'hive'),
-            ('spark.sql.catalog.spark_catalog.type', 'hive'),
-            ('spark.sql.catalog.spark_catalog.uri', 'thrift://localhost:9083'),
-            # ('spark.sql.catalog.spark_catalog.warehouse', 's3a://delta/'),
-            # ('spark.sql.warehouse.dir', 's3a://delta/'),
-            ('spark.sql.execution.pyarrow.enabled', 'true'),
             ('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog'),
             ('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
         ]
@@ -192,18 +190,9 @@ class HudiExperiment(Experiment):
     ):
         self._scale_in_gb = scale_in_gb
         dependencies = f"org.apache.hudi:hudi-spark3.2-bundle_{scala_version}:{hudi_version}"
-        dependencies += AWS_JARS
+        if ENV == 'dev': dependencies += AWS_JARS
         confs = [
             ("spark.jars.packages", dependencies),
-            ('spark.hadoop.fs.s3a.access.key', 'minioadmin'),
-            ('spark.hadoop.fs.s3a.secret.key', 'minioadmin'),
-            ('spark.hadoop.fs.s3a.endpoint', 'http://127.0.0.1:9000'),
-            ('spark.hadoop.fs.s3a.connection.ssl.enabled', 'false'),
-            ('spark.sql.catalog.spark_catalog.type', 'hive'),
-            ('spark.sql.catalog.spark_catalog.uri', 'thrift://localhost:9083'),
-            # ('spark.sql.catalog.spark_catalog.warehouse', 's3a://hudi/'),
-            ('spark.sql.execution.pyarrow.enabled', 'true'),
-            # ('spark.sql.warehouse.dir', 's3a://hudi/'),
             ('spark.sql.extensions', 'org.apache.spark.sql.hudi.HoodieSparkSessionExtension'),
             ('spark.serializer', 'org.apache.spark.serializer.KryoSerializer')
         ]
